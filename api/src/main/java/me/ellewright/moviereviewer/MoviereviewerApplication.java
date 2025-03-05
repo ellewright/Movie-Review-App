cef8d71a6f4940ab9a6fa5a2d442770ad09bac78
Eleanor Wright <elliewright055@gmail.com>
2025-03-05 11:09:19 -0600

    Initialize api using Spring Initializr

---
+++ api/src/main/java/me/ellewright/moviereviewer/MoviereviewerApplication.java
@@ -0,0 +1,13 @@
+package me.ellewright.moviereviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviereviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereviewerApplication.class, args);
+	}
+
+}
